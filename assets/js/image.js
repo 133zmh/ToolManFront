9d5d7f1ce4c0769a12c1b74c32509579a0e40974
胡林凯 <798395174@qq.com>
2020-05-23 02:35:22 +0800

    功能接口使用，文档转换接口读不到选择类型，图片压缩报错没有？路径

---
+++ assets/js/image.js
@@ -0,0 +1,104 @@
+posturl = "http://localhost:9010";
+
+function imageHand() {
+    var $file1 = $("input[name='fileName_hand']").val();
+    if ($file1 == "") {
+        alert("请选择上传的目标文件! ")
+        return false;
+    }
+    var size1 = $("input[name='fileName_hand']")[0].files[0].size;
+    if (size1>104857600) {
+        alert("上传文件不能大于100M!");
+        return false;
+    }
+
+    boo1 = true;
+    var type = "file";
+
+    var formData = new FormData();//这里需要实例化一个FormData来进行文件上传
+    formData.append("picture", $("#fileName_hand")[0].files[0]);
+    formData.append("username", "cj");
+    $.ajax({
+        type : "post",
+        url : posturl+"/image/handwriteingREC",
+        data : formData,
+        processData : false,
+        contentType : false,
+        success : function(data){
+            console.log(data);
+            alert("文件上传成功!");
+            $("#information_back").text(data['data']);
+        }
+    });
+}
+
+function imageCom() {
+    var $file1 = $("input[name='fileName_compress']").val();
+    if ($file1 == "") {
+        alert("请选择上传的目标文件! ")
+        return false;
+    }
+    var size1 = $("input[name='fileName_compress']")[0].files[0].size;
+    if (size1>104857600) {
+        alert("上传文件不能大于100M!");
+        return false;
+    }
+
+    boo1 = true;
+    var type = "file";
+
+    var formData = new FormData();//这里需要实例化一个FormData来进行文件上传
+    formData.append("picture", $("#fileName_compress")[0].files[0]);
+    formData.append("size", $("#img_com_size").val());
+    formData.append("username", "cj");
+    $.ajax({
+        type : "post",
+        url : posturl+"/image/imagecompress",
+        data : formData,
+        processData : false,
+        contentType : false,
+        success : function(data){
+            console.log(data);
+            alert("文件上传成功!");
+            alert($("#img_com_size").val());
+            y='<a download="file" href="'+data.data+'">Download</a>'
+            document.getElementById("download").innerHTML=y;
+        }
+    });
+}
+
+
+function imageRender() {
+    var $file1 = $("input[name='fileName_render']").val();
+    if ($file1 == "") {
+        alert("请选择上传的目标文件! ")
+        return false;
+    }
+    var size1 = $("input[name='fileName_render']")[0].files[0].size;
+    if (size1>104857600) {
+        alert("上传文件不能大于100M!");
+        return false;
+    }
+
+    boo1 = true;
+    var type = "file";
+    var selectValue = $("#img_render option:selected")
+    var formData = new FormData();//这里需要实例化一个FormData来进行文件上传
+    formData.append("style", selectValue.val());
+    formData.append("picture", $("#fileName_render")[0].files[0]);
+    formData.append("username", "cj");
+    $.ajax({
+        type : "post",
+        url : posturl+"/image/imagerendering",
+        data : formData,
+        processData : false,
+        contentType : false,
+        success : function(data){
+            console.log(data);
+            alert("文件上传成功!");
+            alert(selectValue.val());
+            y='<a download="file" href="'+data.data+'">Download</a>'
+            document.getElementById("download").innerHTML=y;
+        }
+    });
+}
