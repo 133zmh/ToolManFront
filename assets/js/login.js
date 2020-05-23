2ec329fb3792bf7eac346edd6831c7b510cc3963
Minecrazy <564193464@qq.com>
2020-05-23 11:20:46 +0800

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,18 +1,22 @@
-posturl = "http://localhost:9010";
+//posturl = "http://localhost:9010";
+
+var postUrl;
 function login(){ //在这里进行ajax 文件上传 用户的信息 
         var $username = $("input[name='username']").val();//用户文件内容(文件)
         var $password = $("input[name='username']").val();
-        // 判断文件是否为空 
+        // 判断用户名是否为空 
         if ($username == "") {
             alert("请填写用户名! ")
+            return false;
+        }// 判断密码是否为空 
+        if ($username == "") {
+            alert("请填写密码! ")
             return false;
         }
 
     $.ajax({
         type : "get",
         url : posturl+"/user/"+$username+"?password="+$password,
-        // processData : false,
-        // contentType : false,
         success : function(data){
             console.log(data);
             alert("成功!");
